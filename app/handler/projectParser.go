28bcf2f36b1a11644c95a3b932cd10a35897baf0
Mark Du <mark_du1@baylor.edu>
2020-06-01 13:26:31 -0500

    Updated creating regexes

--- app/handler/projectParser.go
+++ app/handler/projectParser.go
@@ -1,30 +1,45 @@
 package handler
 
 import (
-	"regexp"
+	"github.com/jinzhu/gorm"
 	"sourcecrawler/app/model"
+	"time"
 )
 
+//Parse project to create log types
 func parseProject(projectRoot string) []model.LogType {
 	// TODO: parse project and create log types
 
 	logTypes := []model.LogType{}
 
+	//Sample log type for testing (will remove once parser is implemented)
+	tempModel := gorm.Model{
+		ID:        1,
+		CreatedAt: time.Time{},
+		UpdatedAt: time.Time{},
+		DeletedAt: nil,
+	}
+	type1 := model.LogType{
+		Model:      tempModel,
+		FilePath:   "./",
+		LineNumber: 1,
+		Regex:      "",
+	}
+	logTypes = append(logTypes, type1)
+
 	//Create regex based on specific log type
-	createRegex(logTypes)
+	for index := range logTypes {
+
+		//TODO: Assuming ID identifies the type of log? (info, error, etc.)
+		//if logTypes[index].ID == 1 {
+		//	logTypes[index].Regex = "^[a-zA-Z0-9]*$"
+		//}
+
+		//Currently all log type messages match this pattern
+		logTypes[index].Regex = "^[a-zA-Z0-9]*$"
+	}
 
 	return logTypes
 }
 
-//Generate list of regexes from log function instances
-func createRegex(logTypes []model.LogType){
 
-	//Assign a regex to each logType
-	for _, elem := range logTypes {
-
-		//TODO: Assuming ID identifies the type of log? (info, error, etc.)
-		if elem.ID == 1 {
-			elem.Regex = "^[a-zA-Z0-9]*$"
-		}
-	}
-}
