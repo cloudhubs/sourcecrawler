9e821daff4216ed4b8574130e055267dd86b1082
nnazo <jacobcurtis2266@gmail.com>
2020-06-01 13:55:11 -0500

    give error response when no log type match is found

--- app/handler/logMatcher.go
+++ app/handler/logMatcher.go
@@ -5,26 +5,34 @@ import (
 
 	"github.com/jinzhu/gorm"
 
+	"fmt"
 	"regexp"
 )
 
-func matchLog(logMessage string, db *gorm.DB) model.LogSourceResponse {
+func matchLog(logMessage string, db *gorm.DB) (*model.LogSourceResponse, error) {
 	// TODO: find log in database
 
 	logTypes := []model.LogType{}
 	db.Find(&logTypes)
 
+	// Initialize default response
+	var response *model.LogSourceResponse
+	err := fmt.Errorf("Could not match any log type to \"%s\"", logMessage)
+
 	// Find the first logType where the logMessage matches the regex
-	response := model.LogSourceResponse{}
 	for _, logType := range logTypes {
 		if regex, err := regexp.Compile(logType.Regex); err == nil {
 			if regex.Match([]byte(logMessage)) {
-				response.LineNumber = logType.LineNumber
-				response.FilePath = logType.FilePath
+				// Found a log type, set values
+				response = &model.LogSourceResponse{
+					LineNumber: logType.LineNumber,
+					FilePath:   logType.FilePath,
+				}
+				err = nil
 				break
 			}
 		}
 	}
 
-	return response
+	return response, err
 }
