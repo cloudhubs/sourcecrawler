37c3b9377887e3326270aeecc167d95c97177041
Mark Du <mark_du1@baylor.edu>
2020-06-01 12:15:12 -0500

    Initial work on creating regexes

--- app/handler/projectParser.go
+++ app/handler/projectParser.go
@@ -1,11 +1,30 @@
 package handler
 
-import "sourcecrawler/app/model"
+import (
+	"regexp"
+	"sourcecrawler/app/model"
+)
 
 func parseProject(projectRoot string) []model.LogType {
 	// TODO: parse project and create log types
 
 	logTypes := []model.LogType{}
 
+	//Create regex based on specific log type
+	createRegex(logTypes)
+
 	return logTypes
+}
+
+//Generate list of regexes from log function instances
+func createRegex(logTypes []model.LogType){
+
+	//Assign a regex to each logType
+	for _, elem := range logTypes {
+
+		//TODO: Assuming ID identifies the type of log? (info, error, etc.)
+		if elem.ID == 1 {
+			elem.Regex = "^[a-zA-Z0-9]*$"
+		}
+	}
 }
