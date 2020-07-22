74dddbe0fbd984da6c40163ea00245c82554397c
nnazo <jacobcurtis2266@gmail.com>
2020-07-22 10:44:33 -0500

    filtering user input, add z3 to ssa test

---
+++ app/cfg/test/example_z3.go
@@ -0,0 +1,25 @@
+package test
+
+func z3example() {
+	x, y, z := -1, -2, 8
+	if x+y+z > 4 {
+	}
+	if x+y < 2 {
+	}
+	if z > 0 {
+	}
+	if x != y {
+	}
+	if x != z {
+	}
+	if y != z {
+	}
+	if x != 0 {
+	}
+	if y != 0 {
+	}
+	if z != 0 {
+	}
+	if x+y == -3 {
+	}
+}
